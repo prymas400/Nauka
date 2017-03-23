38a91d1b28174f6e62284ea1733899715d6a3623
prymas400 <przemek.nauka+github@gmail.com>
2017-03-23 14:04:27 +0100

    first

--- src/pl/pleczuk/Main.java
+++ src/pl/pleczuk/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 5;
+        int b = 5;
+        int c = a + b;
     }
 }
